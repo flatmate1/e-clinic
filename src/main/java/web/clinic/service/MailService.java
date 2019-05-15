a2bad729cc3a021ef5ae4f31d75b5677f1c3c232
test <test@miki-Komputer>
2019-05-15 20:19:01 +0200

    Remove not needed @Service

--- src/main/java/web/clinic/service/MailService.java
+++ src/main/java/web/clinic/service/MailService.java
@@ -2,7 +2,6 @@ package web.clinic.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface MailService {
 	
     void sendSimpleMessage(String to, String subject, String text);
